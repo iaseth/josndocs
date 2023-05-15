866769817c4ec167efcc863ef807420e9052db3e
Ankur Seth <iankurseth@gmail.com>
2023-05-15 16:23:57 +0530

    No need for separated RootLayout inside components
    Hence deleted.
    Added red header with 2 buttons - Try and Docs.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,24 @@
+import Link from 'next/link';
 
 
 
 export default function Home () {
 	return (
-		<main className="min-h-screen p-24">
-			<h1>Home</h1>
-			<footer>
-				<button>Try</button>
-				<button>Docs</button>
-			</footer>
+		<main className="min-h-screen text-center">
+			<header className="bg-red-500 text-white py-6">
+				<header></header>
+				<main className="px-8 py-16 space-y-6">
+					<h1 className="H4">Josn</h1>
+					<h4>Josn is a command line JSON viewer.</h4>
+				</main>
+
+				<footer className="px-4 py-6">
+					<section className="max-w-xs mx-auto grid grid-cols-2 space-x-4">
+						<Link href="/getting-started" className="big-button">Try</Link>
+						<Link href="/getting-started" className="big-button">Docs</Link>
+					</section>
+				</footer>
+			</header>
 		</main>
 	);
 }
