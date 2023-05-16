a772c52e2002b30a72534253d3af9db61b86530b
Ankur Seth <iankurseth@gmail.com>
2023-05-16 21:43:53 +0530

    Updated Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,10 +3,13 @@
 
 export default function Header () {
 	return (
-		<header className="absolute top-0 right-0 px-2 space-x-2 flex">
-			<a href="https://github.com/iaseth/josn" className="header-link">GitHub</a>
-			<a href="https://www.npmjs.com/package/josn-cli" className="header-link">Npm</a>
-			<a href="https://twitter.com/iankurseth" className="header-link">Twitter</a>
+		<header className="absolute w-full top-0 left-0">
+			<section className="max-w-5xl mx-auto px-2 space-x-2 flex">
+				<div className="grow"></div>
+				<a href="https://github.com/iaseth/josn" className="header-link">GitHub</a>
+				<a href="https://www.npmjs.com/package/josn-cli" className="header-link">Npm</a>
+				<a href="https://twitter.com/iankurseth" className="header-link">Twitter</a>
+			</section>
 		</header>
 	);
 }
